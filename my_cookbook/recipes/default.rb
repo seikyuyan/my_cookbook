e7af74b226b9e596ecdf327ff4ed5e5a8386d15f
chzefungthestar <chzefung@thestar.com.my>
2015-02-18 15:05:55 +0800

    Remove Second Deployment

--- my_cookbook/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-[ "/srv/www/dev_cm_lifestyle/current/wp-content/uploads" ].each do |path|
-	directory path do
-		mode 0775
-		owner 'deploy'
-		group 'www-data'
-		recursive true
-		action :create
-	end
-end
