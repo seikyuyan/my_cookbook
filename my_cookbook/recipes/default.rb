757ea070ec43459595f9a1a88bd981ae21dc406f
chzefungthestar <chzefung@thestar.com.my>
2015-02-18 14:08:22 +0800

    Second Deployment Modified

--- my_cookbook/recipes/default.rb
+++ my_cookbook/recipes/default.rb
@@ -1,4 +1,4 @@
-node[:deploy].each do application | deploy |
+node[:deploy].each do |application, deploy|
 	app_root = "#{deploy[:deploy_to]}/current/wp-content/uploads"
 	execute "chmod -R g+rw #{app_root}" do
 	end
